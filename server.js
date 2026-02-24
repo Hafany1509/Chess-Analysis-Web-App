1691d87d637d6257c552f40bdfb432bacc6c3e87
Lê Huy Hải <lehuyhai2005@gmail.com>
2026-02-24 15:48:00 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Phục vụ các file tĩnh trong thư mục public
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server đang chay tai: http://localhost:${PORT}`);
+});
